ff329f5b0e6b7b7bbd6b072c8bf160f7cdd698d9
Deepak Kumar <deepakworldphp86@gmail.com>
2021-06-03 03:47:33 +0530

    change in nginx

--- settings/provision/bootstrap.sh
+++ settings/provision/bootstrap.sh
@@ -161,13 +161,41 @@ EOF
 #*************************Elastic Search *************************/
 # install java
 sudo apt-get install default-jre -y
+sudo apt-get install apt-transport-https
+# install openjdk-7 
+# sudo apt-get purge openjdk*
+# sudo apt-get -y install openjdk-7-jdk
+# install ES
+wget -qO - https://artifacts.elastic.co/GPG-KEY-elasticsearch | sudo apt-key add -
+
+
+echo "deb https://artifacts.elastic.co/packages/7.x/apt stable main" | sudo tee /etc/apt/sources.list.d/elastic-7.x.list
+
+sudo apt-get update && sudo apt-get install elasticsearch
+
+sudo update-rc.d elasticsearch defaults 95 10
+sudo /etc/init.d/elasticsearch start
+# either of the next two lines is needed to be able to access "localhost:9200" from the host os
+sudo echo "network.bind_host: 0" >> /etc/elasticsearch/elasticsearch.yml
+sudo echo "network.host: 0.0.0.0" >> /etc/elasticsearch/elasticsearch.yml
+# enable dynamic scripting
+sudo echo "script.inline: on" >> /etc/elasticsearch/elasticsearch.yml
+sudo echo "script.indexed: on" >> /etc/elasticsearch/elasticsearch.yml
+# enable cors (to be able to use Sense)
+sudo echo "http.cors.enabled: true" >> /etc/elasticsearch/elasticsearch.yml
+sudo echo "http.cors.allow-origin: /https?:\/\/.*/" >> /etc/elasticsearch/elasticsearch.yml
+sudo /etc/init.d/elasticsearch restart
+
+
+
+
 
 # install elasticsearch
-wget https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-7.13.1-amd64.deb
-sudo dpkg -i elasticsearch-7.13.1-amd64.deb
-sudo systemctl enable elasticsearch.service
-sudo systemctl start elasticsearch.service
-sudo service elasticsearch status 
+# wget https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-7.13.1-amd64.deb
+# sudo dpkg -i elasticsearch-7.13.1-amd64.deb
+# sudo systemctl enable elasticsearch.service
+# sudo systemctl start elasticsearch.service
+# sudo service elasticsearch status 
 
 
 
