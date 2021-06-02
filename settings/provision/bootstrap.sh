2d3dcbf3a28cd814f25f97eb755f264db58f51e0
Deepak Kumar <deepakworldphp86@gmail.com>
2021-06-03 03:58:50 +0530

    change in nginx

--- settings/provision/bootstrap.sh
+++ settings/provision/bootstrap.sh
@@ -174,7 +174,7 @@ echo "deb https://artifacts.elastic.co/packages/7.x/apt stable main" | sudo tee 
 sudo apt-get update && sudo apt-get install elasticsearch
 
 sudo update-rc.d elasticsearch defaults 95 10
-sudo /etc/init.d/elasticsearch start
+sudo systemctl start elasticsearch
 # either of the next two lines is needed to be able to access "localhost:9200" from the host os
 sudo echo "network.bind_host: 0" >> /etc/elasticsearch/elasticsearch.yml
 sudo echo "network.host: 0.0.0.0" >> /etc/elasticsearch/elasticsearch.yml
@@ -184,7 +184,7 @@ sudo echo "script.indexed: on" >> /etc/elasticsearch/elasticsearch.yml
 # enable cors (to be able to use Sense)
 sudo echo "http.cors.enabled: true" >> /etc/elasticsearch/elasticsearch.yml
 sudo echo "http.cors.allow-origin: /https?:\/\/.*/" >> /etc/elasticsearch/elasticsearch.yml
-sudo /etc/init.d/elasticsearch restart
+sudo systemctl start elasticsearch
 
 
 
